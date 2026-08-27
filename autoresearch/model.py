88ff5ad37a57cbd702f016b29822d89a786d5088
DeveshParagiri <deveshp@terpmail.umd.edu>
2026-08-27 17:17:10 +0100

    Restore balanced combustion-window strength

--- autoresearch/model.py
+++ autoresearch/model.py
@@ -37,7 +37,7 @@ SEARCH_SPACE: dict[str, dict[str, Any]] = {
 PARAMS = {'annual_scale': 1.73,
  'event_scale_half': 0.003,
  'pathway_mix_w': 0.35,
- 'conditional_allocation_w': 2.0,
+ 'conditional_allocation_w': 1.0,
  'cool_crop_brake': 4.5,
  'wet_forest_brake': 3.0,
  'cold_forest_capacity': 3.0,
